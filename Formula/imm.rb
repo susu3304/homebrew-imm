406375a8b251113895b1c7b45dda2adb34cd7e06
susu3304 <susu3304@ymail.ne.jp>
2026-05-30 12:17:39 +0900

    Update IMM formula to 0.2.1

--- Formula/imm.rb
+++ Formula/imm.rb
@@ -1,21 +1,21 @@
 class Imm < Formula
   desc "Insane Marmot Matrix native runtime"
-  homepage "https://github.com/susu3304/InsaneMarmotMatrixLanguage"
-  version "0.2.1+git16.a6db498c2cd1"
+  homepage "https://github.com/susu3304/IMM"
+  version "0.2.1"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/susu3304/imm-installers/releases/download/imm-v0.2.1-git16-a6db498c2cd1/imm-macos-arm64.tar.gz"
-      sha256 "ee97c92505667dc8d4d438913b6f57c917201f60746a770b4da06928582d446d"
+      url "https://github.com/susu3304/IMM/releases/download/v0.2.1/imm-macos-arm64.tar.gz"
+      sha256 "9fdaad24bba75fa69a0460e8abfa14e398b7dcab19e8d5cc0c8afd65e393d015"
     else
-      url "https://github.com/susu3304/imm-installers/releases/download/imm-v0.2.1-git16-a6db498c2cd1/imm-macos-x64.tar.gz"
-      sha256 "373e1201eb75f0a576653aca5f95123a7ab38fc457af8b50c68ca60f9f88a0d0"
+      url "https://github.com/susu3304/IMM/releases/download/v0.2.1/imm-macos-x64.tar.gz"
+      sha256 "5f769899aba0176637a2ae0f71b4b8e3bcf8136943d34d8a3711329e6c3457a2"
     end
   end
 
   def install
-    bin.install "bin/imm"
+    bin.install "imm/bin/imm"
   end
 
   test do
